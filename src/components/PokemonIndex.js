cee720537e1bb0b701c2cfcb20ba144f4bbb03ca
Alexander Schelchere <ascellie@gmail.com>
2019-08-12 17:33:27 -0400

    all deliverables except bonus (I think)

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,56 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from 'react';
+import PokemonCollection from './PokemonCollection';
+import PokemonForm from './PokemonForm';
+import { Search } from 'semantic-ui-react';
+import _ from 'lodash';
+
+const API = 'http://localhost:3000/pokemon';
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemonList: [],
+    pokemonListCopy: [],
+    searchTerm: ''
+  };
+
+  componentDidMount() {
+    this.fetchPokemon()
+  }
+
+  fetchPokemon = () =>
+    fetch(API)
+      .then(res => res.json())
+      .then(this.loadPokemon);
+
+  loadPokemon = pokemonData => {
+    this.setState({ pokemonList: [...pokemonData], pokemonListCopy:[...pokemonData] })
+  };
+
+  handleChange = searchTerm => {
+    let filteredList = this.state.pokemonList.filter(pokemon => pokemon.name.includes(searchTerm))
+    this.setState({
+      searchTerm,
+      pokemonListCopy: filteredList
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search
+          onSearchChange={event => this.handleChange(event.target.value)}
+          value={this.state.searchTerm}
+          showNoResults={false}
+        />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemonList={this.state.pokemonListCopy} />
         <br />
         <PokemonForm />
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
