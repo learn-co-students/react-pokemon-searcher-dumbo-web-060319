73976240c9b6580b4aff1e82bd71e9ca121d81dc
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-11 23:10:06 -0400

    got the fetch to work properly for the pokemon, now have to work on the cards

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,12 +3,23 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+	state = {
+    pokemons: []
+  }
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+				{this.state.pokemons.map(pokemon => <PokemonCard pokemon={pokemon} key={pokemon.id}/>)}
       </Card.Group>
     )
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(pokemons => this.setState({pokemons}))
   }
 }
 
