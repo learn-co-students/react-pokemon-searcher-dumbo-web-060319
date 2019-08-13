fb867392a76d3001004ee78f7b7ffb97b2fbbb25
Justin Hutsell <hutselljustin@gmail.com>
2019-08-13 10:12:21 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,9 +4,12 @@ import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
   render() {
+    console.log(this.props)
+    let arrayOfPokemons = this.props.allPokemon.map(pokemon => <PokemonCard pokemon={ pokemon } key={ pokemon.id }/>)
+    
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        { arrayOfPokemons }
       </Card.Group>
     )
   }
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -4,17 +4,55 @@ import PokemonForm from './PokemonForm'
 import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
+const API = "http://localhost:3000/pokemon"
+
 class PokemonPage extends React.Component {
+
+  state = {
+    allPokemon: [],
+    copiedArray: [],
+    term: ''
+  }
+
+
+
+  componentDidMount() {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ allPokemon: data, copiedArray: data })
+      })
+  };
+
+  handleSearch = (term) => {
+    let newArrayOfPokemons = this.state.allPokemon.filter(pokemon => {
+      return pokemon.name.includes(term)
+    })
+    
+    this.setState({
+      term: term,
+      copiedArray: newArrayOfPokemons
+    })
+  }
+
+  addPokemon = (newPokemon) => {
+    let addedPokemon = [...this.state.allPokemon, newPokemon]
+    this.setState({
+      allPokemon: addedPokemon,
+      copiedArray: addedPokemon
+    })
+  }
+    
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={ (event) => this.handleSearch(event.target.value) } value={ this.state.term } showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection allPokemon={ this.state.copiedArray }/>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={ this.addPokemon } />
       </div>
     )
   }
