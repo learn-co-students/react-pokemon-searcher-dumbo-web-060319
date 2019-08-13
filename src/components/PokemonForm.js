f5fb138456a532a198dea3b98265cbb4d30fdb1d
Alexander Schelchere <ascellie@gmail.com>
2019-08-12 21:00:03 -0400

    full form - with validations

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,34 +1,75 @@
-import React from 'react'
-import { Form } from 'semantic-ui-react'
+import React from 'react';
+import { Form } from 'semantic-ui-react';
 
 class PokemonForm extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       name: '',
       hp: '',
       frontUrl: '',
       backUrl: ''
-    }
+    };
   }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let { name, hp, frontUrl, backUrl } = this.state;
+    // Handle blanks
+    if (name === '' || hp === '' || frontUrl === '' || backUrl === '')
+      return alert('INVALID INPUT');
+    // Set our pokemon
+    let pokemon = { ...this.state };
+    this.props.handlePokemonPost(pokemon);
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={this.handleSubmit} onChange={this.handleInputChange}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input
+              fluid
+              label="Name"
+              placeholder="Name"
+              name="name"
+              value={this.state.name}
+            />
+            <Form.Input
+              fluid
+              label="hp"
+              placeholder="hp"
+              name="hp"
+              value={this.state.hp}
+            />
+            <Form.Input
+              fluid
+              label="Front Image URL"
+              placeholder="url"
+              name="frontUrl"
+              value={this.state.frontUrl}
+            />
+            <Form.Input
+              fluid
+              label="Back Image URL"
+              placeholder="url"
+              name="backUrl"
+              value={this.state.backUrl}
+            />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
       </div>
-    )
+    );
   }
 }
 
-export default PokemonForm
+export default PokemonForm;
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -14,7 +14,7 @@ class PokemonPage extends React.Component {
   };
 
   componentDidMount() {
-    this.fetchPokemon()
+    this.fetchPokemon();
   }
 
   fetchPokemon = () =>
@@ -23,16 +23,101 @@ class PokemonPage extends React.Component {
       .then(this.loadPokemon);
 
   loadPokemon = pokemonData => {
-    this.setState({ pokemonList: [...pokemonData], pokemonListCopy:[...pokemonData] })
+    this.setState({
+      pokemonList: [...pokemonData],
+      pokemonListCopy: [...pokemonData]
+    });
   };
 
   handleChange = searchTerm => {
-    let filteredList = this.state.pokemonList.filter(pokemon => pokemon.name.includes(searchTerm))
+    let filteredList = this.state.pokemonList.filter(pokemon =>
+      pokemon.name.includes(searchTerm)
+    );
     this.setState({
       searchTerm,
       pokemonListCopy: filteredList
-    })
+    });
+  };
+
+  handlePokemonPost = pokemon => {
+    // return if a pokemon already exsists
+    if (this.hasPokemon(pokemon)) return alert('POKEMON ALREADY EXSISTS');
+    // Fetch One pokemon from pokeApi
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+      .then(res => res.json())
+      // then set that pokemon to a variable, foundPokemon
+      .then(pokemonData => {
+        // Destructure pokemonData to match what we want in Our database
+        let {
+          id,
+          name,
+          height,
+          weight,
+          abilities,
+          moves,
+          stats,
+          types,
+          sprites
+        } = pokemonData;
+        // newPokemon will be a pokemon with those attributes
+        let newPokemon = {
+          id,
+          name,
+          height,
+          weight,
+          abilities: abilities.map(oneAbility => oneAbility.ability.name),
+          moves,
+          stats: stats.map(oneStat => {
+            let statObj = { name: oneStat.stat.name, value: oneStat.base_stat };
+            return statObj;
+          }),
+          types: types.map(oneType => oneType.type.name),
+          sprites: { front: sprites.front_default, back: sprites.back_default }
+        };
+        // Post that pokemon to our database
+        this.postPokemon(newPokemon);
+      })
+      .catch(err => {
+        alert('COULD NOT FIND POKEMON');
+        return console.error(err.stack);
+      });
+    console.log('Submitted!');
+  };
+
+  postPokemon = pokemon => {
+    const url = 'http://localhost:3000/pokemon'
+    fetch(url, {
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(pokemon)
+    }).then(res => res.json())
+      .then(this.slapPokemonOnDom)
+      .catch(err => {
+        console.log(`COULD NOT POST TO ${url}`)
+      })
   }
+
+  slapPokemonOnDom = pokeData => {
+    let {pokemonList, pokemonListCopy} = this.state
+    this.setState({
+      pokemonList: [...pokemonList, pokeData],
+      pokemonListCopy: [...pokemonListCopy, pokeData]
+    });
+  }
+
+  // If the pokemon is in our list
+  hasPokemon = pokemon => {
+    let foundPokemon = this.state.pokemonList.find(
+      onePokemon => onePokemon.name.toLowerCase() === pokemon.name.toLowerCase()
+    );
+    if (foundPokemon) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   render() {
     return (
@@ -47,7 +132,7 @@ class PokemonPage extends React.Component {
         <br />
         <PokemonCollection pokemonList={this.state.pokemonListCopy} />
         <br />
-        <PokemonForm />
+        <PokemonForm handlePokemonPost={this.handlePokemonPost} />
       </div>
     );
   }
